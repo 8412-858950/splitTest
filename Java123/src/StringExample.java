36ab3bd9cd1af3888fd223293851bafa09dca58b
Pallavi Bharat Naikwade <pallavi.naikwade13@gmail.com>
2019-06-08 12:56:36 +0530

    add devopes

--- Java123/src/StringExample.java
+++ Java123/src/StringExample.java
@@ -1,7 +1,7 @@
 
 public class StringExample { 
     public static void main(String args[]){  
-    String s1="java";//creating string by java string literal  
+    String s1="devopes";//creating string by java string literal  
     char ch[]={'s','t','r','i','n','g','s'};  
     String s2=new String(ch);//converting char array to string  
     String s3=new String("example");//creating java string by new keyword  
